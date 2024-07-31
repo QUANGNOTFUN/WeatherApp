bd9d075dffea3b2ad77bb83ee9c260c65dc0b77f
nguyenhonghoang2004@gmail.com <nguyenhonghoang2004@gmail.com>
2024-07-31 21:47:13 +0700

    edit22

--- WeatherApp/Form1.Designer.cs
+++ WeatherApp/Form1.Designer.cs
@@ -37,14 +37,14 @@
             this.webBrowser1.Location = new System.Drawing.Point(0, 0);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(1025, 435);
+            this.webBrowser1.Size = new System.Drawing.Size(255, 494);
             this.webBrowser1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1025, 435);
+            this.ClientSize = new System.Drawing.Size(255, 494);
             this.Controls.Add(this.webBrowser1);
             this.Name = "Form1";
             this.Text = "Form1";
